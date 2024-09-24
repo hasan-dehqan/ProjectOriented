0bccb365117b33fc3c6faa8995a5f0c91b1256de
Hassan Dehqan <hsn160dq@gmail.com>
2024-09-24 13:27:23 +0330

    Project in progress.

---
+++ src/_components/types/aligment.type.ts
@@ -0,0 +1,2 @@
+export type Aligment = "left" | "right" | "center" | "justify";
+
---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+// eslint-disable-next-line @next/next/no-async-client-component
+export default async function Error() {
+    return (
+        <div className="text-3xl text-center text-red-600">
+            Error...
+        </div>
+    )
+}
